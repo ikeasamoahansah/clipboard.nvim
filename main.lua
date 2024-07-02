bdf4ca1cb6aada704d1ed39d44d17cb0fb3cac6e
Ike <dylanraspino264@gmail.com>
2024-07-02 00:21:51 +0000

    transfer to plugin

--- main.lua
+++ main.lua
@@ -5,21 +5,36 @@ local config = require("telescope.config").values
 local previewers = require("telescope.previewers")
 local utils = require("telescope.previewers.utils")
 
+local clipboard_history = {}
+
 local function get_clipboard_cont()
     local clipboard = vim.fn.getreg("+")
     return clipboard
 end
 
-M.show_clipboard = function()
+local function update_clipboard_history()
+    local clipboard_content = get_clipboard_cont()
+    if clipboard_content ~= clipboard_history[1] then
+        table.insert(clipboard_history, 1, clipboard_content)
+    end
+    if #clipboard_history > 5 then
+        table.remove(clipboard_history)
+    end
+end
+
+update_clipboard_history()
+
+M.show_clipboard = function(opts)
+    opts = opts or {}
     pickers
         .new(opts, {
             finder = finders.new_table({
-                results = {"Clipboard content"},
+                results = clipboard_history,
 
                 entry_maker = function(entry)
                     return {
                         value = entry,
-                        display = entry,
+                        display = entry:sub(1, 5),
                         ordinal = entry
                     }
                 end
@@ -37,7 +52,7 @@ M.show_clipboard = function()
                         false,
                         vim.tbl_flatten({
                             "``` lua",
-                            vim.split(clipboard_content, "\n"),
+                            vim.split(entry.value, "\n"),
                             "```",
                         })
                     )
@@ -48,6 +63,17 @@ M.show_clipboard = function()
         :find()
 end
 
+-- Set up an autocommand to update the clipboard history whenever the clipboard content changes
+--vim.cmd([[
+--    augroup ClipboardHistory
+--        autocmd!
+--        autocmd TextYankPost * lua require("clipboard_history").update_clipboard_history()
+--    augroup END
+--]])
+
+-- Export the update_clipboard_history function so it can be called by the autocommand
+M.update_clipboard_history = update_clipboard_history()
 M.show_clipboard()
 
 return M
+
